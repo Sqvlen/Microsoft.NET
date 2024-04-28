29648a0e8ee8f00c0a254aa290d80a39ccac0c78
Sqvlen <kirillnesterenka@gmail.com>
2024-04-28 16:23:49 +0300

    fix applicant entity

---
+++ Lab4/Lab4.Infrastructure/Applicant/ApplicantEntity.cs
@@ -0,0 +1,23 @@
+﻿using Lab4.Infrastructure.Applicant.Enums;
+using Lab4.Infrastructure.Database;
+
+namespace Lab4.Infrastructure.Applicant;
+
+public class ApplicantEntity : BaseEntity<long>
+{
+    public string LastName { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string MiddleName { get; set; }
+    
+    public DateTime DateOfBirthday { get; set; } // replace to DateOnly
+
+    public Dictionary<string, float> ExamsResults { get; } = new();
+
+    public ICollection<string> Specialties { get; } = new List<string>();
+    
+    public EducationLevel EducationLevel { get; set; }
+    
+    public StudyForm StudyForm { get; set; }
+}
