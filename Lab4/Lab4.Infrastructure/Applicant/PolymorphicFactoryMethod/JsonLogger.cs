6f9347f98fdb75f9c8f716dcca61c84529de66d4
Sqvlen <kirillnesterenka@gmail.com>
2024-04-28 16:23:04 +0300

    add polymorphic fabric method pattern for logging

---
+++ Lab4/Lab4.Infrastructure/Applicant/PolymorphicFactoryMethod/JsonLogger.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Lab4.Infrastructure.Applicant.PolymorphicFactoryMethod;
+
+public class JsonLogger : ILogger
+{
+    public void Save(ApplicantEntity applicantEntity, string fileName)
+    {
+        var json = JsonConvert.SerializeObject(applicantEntity);
+        
+        File.WriteAllText(fileName + ".json", json);
+
+        Console.WriteLine($"Saved as JSON - {fileName}");
+    }
+}
