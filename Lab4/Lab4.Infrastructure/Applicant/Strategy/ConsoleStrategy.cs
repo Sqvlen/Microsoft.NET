9140d5b334989ad42633284502cef3dba14e1d9c
Sqvlen <kirillnesterenka@gmail.com>
2024-04-28 16:22:27 +0300

    delete strategy

--- Lab4/Lab4.Infrastructure/Applicant/Strategy/ConsoleStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab4.Infrastructure.Applicant.Strategy;
-
-public class ConsoleStrategy : IApplicantStrategy
-{
-    public void DoLog(ApplicantModel applicantModel)
-    {
-        Console.WriteLine(applicantModel.ToString());
-    }
-}
