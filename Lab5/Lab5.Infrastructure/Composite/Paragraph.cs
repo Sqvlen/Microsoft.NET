13f455921982541b237116c8de614cf359287d18
Sqvlen <svs83.com@gmail.com>
2024-05-14 12:02:28 +0300

    implement composite pattern

---
+++ Lab5/Lab5.Infrastructure/Composite/Paragraph.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Lab5.Infrastructure.Composite;
+
+public class Paragraph : Component
+{
+    private readonly List<Component> _children = new();
+    
+    public override string Operation()
+    {
+        var result = new StringBuilder();
+
+        foreach (var child in _children)
+        {
+            result.AppendLine(child.Operation() + ".");
+        }
+        
+        return result.ToString();
+    }
+    
+    public override void Add(Component component)
+    {
+        _children.Add(component);
+    }
+
+    public override void Remove(Component component)
+    {
+        _children.Remove(component);
+    }
+
+    public override bool IsComposite()
+    {
+        return true;
+    }
+}
